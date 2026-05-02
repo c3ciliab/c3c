1597480c80201b847476dc534eece8b5e5aea4ef
c3ciliab <c3cilia.b@gmail.com>
2026-05-02 17:56:24 +0200

    feature: next page btn

--- src/app/features/landing/sections/contact-section/contact-section.component.ts
+++ src/app/features/landing/sections/contact-section/contact-section.component.ts
@@ -1,18 +1,25 @@
 import { Component, inject } from '@angular/core';
 import { I18nService } from '../../../../core/services/i18n.service';
+import { SectionScrollService } from '../../../../core/services/section-scroll.service';
 import { SectionAnchorDirective } from '../../../../shared/directives/section-anchor.directive';
+import { NextSectionButtonComponent } from '../../../../shared/ui/next-section-button/next-section-button.component';
 
 @Component({
   selector: 'app-contact-section',
   standalone: true,
-  imports: [SectionAnchorDirective],
+  imports: [SectionAnchorDirective, NextSectionButtonComponent],
   templateUrl: './contact-section.component.html',
   styleUrl: './contact-section.component.scss',
 })
 export class ContactSectionComponent {
   private readonly i18n = inject(I18nService);
+  private readonly sectionScroll = inject(SectionScrollService);
 
   t(key: string): string {
     return this.i18n.t(key);
+  }
+
+  goTo(sectionId: string): void {
+    this.sectionScroll.scrollToSection(sectionId);
   }
 }
--- src/app/shared/ui/next-section-button/next-section-button.component.ts
+++ src/app/shared/ui/next-section-button/next-section-button.component.ts
@@ -10,16 +10,34 @@ import { SectionScrollService } from '../../../core/services/section-scroll.serv
   styleUrl: './next-section-button.component.scss',
 })
 export class NextSectionButtonComponent {
-  private readonly sectionScroll = inject(SectionScrollService);
   private readonly i18n = inject(I18nService);
+  private readonly sectionScroll = inject(SectionScrollService);
 
   @Input({ required: true }) currentSectionId!: string;
+  @Input() labelKey = 'common.next';
+  @Input() goToSectionId?: string;
+  // @Input() showIcon = true;
+  // @Input() direction: 'down' | 'up' = 'down';
+  @Input() direction: 'down' | 'up' | 'none' = 'down';
+  @Input() isBackToTop = false;
 
   next(): void {
+    if (this.goToSectionId) {
+      this.sectionScroll.scrollToSection(this.goToSectionId);
+      return;
+    }
+
     this.sectionScroll.goToNextSection(this.currentSectionId);
   }
 
   label(): string {
-    return this.i18n.t('common.next');
+    // return this.i18n.t('common.next');
+    return this.i18n.t(this.labelKey);
+  }
+
+  icon(): string {
+  if (this.direction === 'up') return '↑';
+  if (this.direction === 'down') return '↓';
+    return '';
   }
 }
